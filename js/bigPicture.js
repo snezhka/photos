1cf661c6a8dc0b6dcdc7d7fad4fee5a5e9814c33
snezhka <ekaterina.latsko@gmail.com>
2023-11-28 15:24:19 +0200

    fix comments count

--- js/bigPicture.js
+++ js/bigPicture.js
@@ -4,9 +4,13 @@ const likesCount = document.querySelector('.likes-count');
 const socialComments = document.querySelector('.social__comments');
 const bigPicture = document.querySelector('.big-picture');
 const socialCaption = document.querySelector('.social__caption');
-const commentsCount = document.querySelector('.social__comment-count');
 const body = document.querySelector('body');
 const close = document.querySelector('#picture-cancel');
+const commentsLoader = document.querySelector('.social__comments-loader');
+const commentsShown = document.querySelector('.comments-shown');
+const commentsCount = document.querySelector('.comments-count');
+let commentsArr = [];
+let commentsNum = 0;
 
 export function generateBigPicture(evt) {
     const smallPicture = evt.target;
@@ -17,10 +21,29 @@ export function generateBigPicture(evt) {
         bigPicture.classList.remove('hidden');
         bigPictureImg.src = evt.target.src;
         likesCount.textContent = targetElement.querySelector('.picture__likes').textContent;
-        const commentsArr = JSON.parse(comments.textContent);
-        socialComments.innerHTML = commentsArr.map(comment => `<li class="social__comment"><img class="social__picture" src="${comment.avatar}" alt="${comment.name}" width="35" height="35"> <p class="social__text">${comment.message}</p></li>`).join('');
-        commentsCount.classList.add('hidden');
+        socialComments.innerHTML = '';
+        commentsArr = JSON.parse(comments.textContent);
+        commentsNum = commentsArr.slice().length;
+        commentsLoader.classList.remove('hidden');
+        generateComments(commentsNum);
         body.classList.add('.modal-open');
+        // commentsLoader.classList.toggle('hidden');
+    }
+}
+
+function generateComments(commentsNum) {
+    commentsCount.innerHTML = commentsNum;
+    let i = Math.min(5, commentsArr.length);
+    let shown = 0;
+    while (i != 0) {
+        socialComments.innerHTML += `<li class="social__comment"><img class="social__picture" src="${commentsArr[0].avatar}" alt="${commentsArr[0].name}" width="35" height="35"> <p class="social__text">${commentsArr[0].message}</p></li>`;
+        commentsShown.innerHTML = ++shown;
+        commentsArr.shift();
+        i--;
+    }
+    if (commentsArr.length == 0) {
+        commentsLoader.classList.add('hidden');
+        commentsShown.innerHTML = commentsNum;
     }
 }
 
@@ -36,3 +59,4 @@ document.addEventListener('keydown', evt => {
         closeBigPicture();
     }
 });
+commentsLoader.addEventListener('click', () => generateComments(commentsNum));
