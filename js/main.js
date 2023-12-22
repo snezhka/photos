6e163b30cc4216862736286147cec397f57bf217
Kateryna Latsko <ekaterina.latsko@gmail.com>
2023-12-22 14:57:51 +0200

    finished sending form without validation

--- js/main.js
+++ js/main.js
@@ -1,13 +1,9 @@
 import { generatePictures } from './generatePictures.js';
 import { generateBigPicture } from './bigPicture.js';
-import { validateForm } from './uploadFileForm.js';
 const picturesContainer = document.querySelector('.pictures.container');
-const uploadSubmit = document.querySelector('#upload-submit');
 
 fetch('http://localhost:8080/photos').then(data => data.json()).then(photos => {
     generatePictures(photos)
 }).catch(err => console.log(err.message));
 
 picturesContainer.addEventListener('click', (evt) => generateBigPicture(evt));
-
-uploadSubmit.addEventListener('click', (evt) => validateForm(evt));
